be2f8394a8dba2a8a2387cb9ad12f93d191f9ab4
aiborra11 <iborra.11@gmail.com>
2020-04-11 13:18:24 +0200

    final push after documenting fx

--- source/crypto_scraper_updator.py
+++ source/crypto_scraper_updator.py
@@ -4,6 +4,16 @@ from source.crypto_scraper import csv_creator
 
 
 def data_obtainer(interval, crypto, name):
+    """Iterates through the dates list collecting the data for the specified cryptocurrency.
+    The dates list contains the dates from which we are interested in collecting data.
+    Arguments:
+        interval {[list]} -- interval of dates we are going to collect.
+        name {[str]} -- how we want to name our csv file.
+        crypto {[str]} -- crypto data we are willing to collect.
+
+    Returns:
+        [csv] -- gzip file with the data for the desired cryptocurrency.
+    """
     for date in interval:
         print(date)
         dataset = pd.read_csv(f'https://s3-eu-west-1.amazonaws.com/public.bitmex.com/data/trade/{date}.csv.gz')
