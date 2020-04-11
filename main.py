f22c4bb2da22ae5edc7dbf6814f045be015cafe3
aiborra11 <iborra.11@gmail.com>
2020-04-11 13:24:38 +0200

    documenting fx

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ import time
 from source.crypto_scraper import *
 
 
-def main(crypto):
+def main(crypto, day1):
+    """Pipeline to execute the functions from the source.crypto_scraper script.
+
+    Arguments:
+        crypto {[str]} -- crypto we are interested in collecting data.
+        day1 {[str]} -- date from which we want to start to collect data.
+
+    Returns:
+        [gzip] -- gzip file containing the data for the desired crypto and the desired dates.
+    """
     print('Preparing your data...')
-    dates = dates_converter(day1='20141122')
+    dates = dates_converter(day1)
     print('Charging first csv...')
     data1 = pd.DataFrame(data_obtainer(dates[:500], crypto, 'data1gzip'))
     time.sleep(15)
@@ -18,5 +27,13 @@ def main(crypto):
     time.sleep(15)
     data4 = pd.DataFrame(data_obtainer(dates[1500:], crypto, 'data4gzip'))
 
-if __name__ == "__main__":                #Feel free to replace 'XBTUSD' by the desired crypto
-    main(crypto='XBTUSD')
+
+
+
+if __name__ == "__main__":
+    """
+    
+    FEEL FREE TO MODIFY THIS STRINGS TO COLLECT THE DESIRED DATA
+    
+    """
+    main(crypto='XBTUSD', day1='20141122')
